482aa7c5dca50a4dfc21289afc6a54a1a0e1299d
rzldev <amrizalrf@gmail.com>
2022-09-18 20:30:03 +0800

    SVM Kernels

---
+++ S3-Classification/Pt4-SVMKernels/svm_rbf.R
@@ -0,0 +1,62 @@
+## SVM RBF
+
+## Import the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+
+## Splitting the dataset into the training set and test set
+# install.packages('caTools')
+library(caTools)
+split = sample.split(dataset$Purchased, SplitRatio = .8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+## Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+## Fitting the SVM model on the training set
+# install.packages('e1071')
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'radial')
+
+## Predicting the test set results
+y_pred = predict(classifier, newdata = test_set[-3])
+
+## Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+print(cm)
+
+## Visualizing the training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = .01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = .01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, 3],
+     main = 'RBF (training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+## Visualizing the test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = .01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = .01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, 3],
+     main = 'RBF (test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
